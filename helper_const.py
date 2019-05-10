f39ceb4ec1637706079da4a9b1e930c5a8e025d7
Mozai233 <47612561+Mozai233@users.noreply.github.com>
2019-05-11 07:50:29 +0800

    Update helper_const.py

--- helper_const.py
+++ helper_const.py
@@ -5,8 +5,8 @@
 """ helper_const.py """
 """ Copyright 2018, Jogle Lew """
 
-BOT_TOKEN = 'Bot Token Here'
-BOT_OWNER = [Bot Owner 1 UserID, Bot Owner 2 UserID]
+BOT_TOKEN = '818612258:AAF2yhhZTdOurpDhSP0I0yvrn3alj0wjt7k'
+BOT_OWNER = [476457008]
 MIN_REFRESH_INTERVAL = 5
 MODULE_NAME = ['start_cmd', 'help_cmd', 'cancel_cmd', 'sql_cmd', 'clean_cmd', 'register_cmd', 'option_cmd', 'channel_msg', 'private_msg', 'callback_query']
 DATABASE_DIR = "."
